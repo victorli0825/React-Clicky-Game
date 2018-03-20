cbdcb568405597d4e48cbf8035e365e3b73849a2
SASA <apple@sasas-air.attlocal.net>
2018-03-19 22:52:34 -0700

    upload more

---
+++ src/components/GameInstructios/GameInstructions.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./GameInstructions.css";
+
+const  GameInstructions = props => (
+    <div className="col-md-12">
+        <div className="gameInstructions">
+            <h1>GAME INSTRUCTIONS:</h1>
+            <h2>Click on the images but never click on the same one twice.</h2>
+            <h2>Or you will lose!</h2>
+        </div>
+    </div>
+
+);
+  
+export default GameInstructions;
